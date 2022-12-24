f950113a9cc1f1b670b37f63330bfd6dfb5287f7
kakashiwh <m.rishabhsoni@gmail.com>
2022-12-24 10:24:11 +0530

    Add files via upload
    4th commit

--- index.js
+++ index.js
@@ -21,18 +21,33 @@ app.use(cors());
 app.use('/posts',postRouters);
 const PORT = process.env.PORT || 5000;
 //const CONNECTION_URL='mongodb+srv://rishabhsoni:rishabhsoni@cluster0.lwq6dqg.mongodb.net/?retryWrites=true&w=majority';
-
-
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.CONNECTION_URL);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
 
 mongoose.set("strictQuery", false);
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+      console.log("listening for requests");
+  })
+})
+
+/*
 mongoose.connect(process.env.CONNECTION_URL,{ useNewUrlParser : true , useUnifiedTopology: true})
      .then(() => app.listen(PORT,() => console.log(`Server running on port: ${PORT}`)))
      .catch((error) => console.log(error));
-
-     app.use(express.static(path.join(__dirname, "./client/build")));
+ */
+     app.use(express.static(path.join(__dirname, "../client/build")));
      app.get("*", function (_, res) {
        res.sendFile(
-         path.join(__dirname, "./client/build/index.html"),
+         path.join(__dirname, "../client/build/index.html"),
          function (error) {
            res.status(500).send(error.message);
          }
