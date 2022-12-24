921d148e851eaaa406ce11879c2639c6dfeb22e4
kakashiwh <m.rishabhsoni@gmail.com>
2022-12-24 11:16:45 +0530

    Update index.js
    7th commit challo my leg is paining

--- index.js
+++ index.js
@@ -33,10 +33,10 @@ app.use(express.static(path.join(__dirname, "./client/build")));
 const PORT = process.env.PORT || 5000;
 //const CONNECTION_URL='mongodb+srv://rishabhsoni:rishabhsoni@cluster0.lwq6dqg.mongodb.net/?retryWrites=true&w=majority';
 
-mongoose.set("strictQuery", false);
+
 
 mongoose.connect(process.env.CONNECTION_URL,{ useNewUrlParser : true , useUnifiedTopology: true})
      .then(() => app.listen(PORT,() => console.log(`Server running on port: ${PORT}`)))
      .catch((error) => console.log(error));
- 
+ mongoose.set("strictQuery", false);
      
