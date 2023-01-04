6468b3d7592b7ea392ed06817823b38057f519d5
kakashiwh <m.rishabhsoni@gmail.com>
2023-01-04 09:22:01 +0530

    Add files via upload
    version 2.0

--- controllers/posts.js
+++ controllers/posts.js
@@ -9,12 +9,12 @@ export const getPosts = async (req,res) => {
      } catch (error) {
           res.status(404).json( {message: error.message});
      }
-}
+};
 
 export const createPost = async (req, res) => {
      const post = req.body;
      
-     const newPost = new PostMessage(post);
+     const newPost = new PostMessage({...post, creator: req.userId, createdAt: new Date().toISOString()});
      
      try {
          await newPost.save(); 
@@ -23,7 +23,7 @@ export const createPost = async (req, res) => {
      } catch (error) {
           res.status(409).json({message: error.message});
      }
-}
+};
 
 export const updatePost = async (req,res) => {
      const { id:_id } = req.params;
@@ -33,7 +33,7 @@ export const updatePost = async (req,res) => {
      
      const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post,_id}, { new:true });
      res.json(updatedPost);
-}
+};
 
 export const deletePost = async (req,res) => {
      const { id } = req.params;
@@ -43,14 +43,26 @@ export const deletePost = async (req,res) => {
      await PostMessage.findByIdAndRemove(id);
 
      res.json( {message: 'Post deleted sucessfully'});
-}
+};
 
 export const likePost = async (req,res) => {
      const { id } = req.params;
 
+     if(!req.userId) return res.json({ message: "Unauthenticated"});
+
      if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
 
      const post = await PostMessage.findById(id);
-     const updatedPost = await PostMessage.findByIdAndUpdate(id, {likeCount: post.likeCount + 1}, { new: true})
+     
+     const index= post.likes.findIndex((id)=> id === String(req.userId));
+     
+     if(index === -1){
+          post.likes.push(req.userId);
+     }else{
+          post.likes = post.likes.filter((id)=> id !== String(req.userId));
+     }
+
+     const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true});
+     
      res.json(updatedPost);
-}
+};
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import path from 'path';
 import postRouters from './routes/posts.js';
+import userRouters from './routes/users.js';
 import {fileURLToPath} from 'url';
 
 const app = express();
@@ -19,24 +20,25 @@ app.use(bodyParser.urlencoded({limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use('/posts',postRouters);
-app.disable('etag');
-app.use(express.static(path.join(__dirname, "./client/build")));
-     app.get("*", function (_, res) {
-       res.sendFile(
-         path.join(__dirname, "./client/build/index.html"),
-         function (error) {
-           res.status(500).send(error.message);
-         }
-       );
-     });
+app.use('/user', userRouters);
 
 const PORT = process.env.PORT || 5000;
+//const CONNECTION_URL='mongodb+srv://rishabhsoni:rishabhsoni@cluster0.lwq6dqg.mongodb.net/?retryWrites=true&w=majority';
 
 
- mongoose.set("strictQuery", false);
-     
+app.use(express.static(path.join(__dirname, "../client/build")));
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "../client/build/index.html"),
+    function (error) {
+      res.status(500).send(error.message);
+    }
+  );
+});
 
+mongoose.set("strictQuery", false);
 mongoose.connect(process.env.CONNECTION_URL,{ useNewUrlParser : true , useUnifiedTopology: true})
      .then(() => app.listen(PORT,() => console.log(`Server running on port: ${PORT}`)))
      .catch((error) => console.log(error));
 
+    
