b0484a336b6f71436d385e0f98455b71dc5c871a
kakashiwh <m.rishabhsoni@gmail.com>
2023-01-18 13:36:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(cors());
 app.use('/posts',postRouters);
 app.use('/user', userRouters);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 //const CONNECTION_URL='mongodb+srv://rishabhsoni:rishabhsoni@cluster0.lwq6dqg.mongodb.net/?retryWrites=true&w=majority';
 
 
